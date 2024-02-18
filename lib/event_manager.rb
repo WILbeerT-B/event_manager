f20bca63a78288a963a42339aed7e6b880b29707
WILbeerT-B <wilbertbundalian@gmail.com>
2024-02-17 17:01:44 -0800

    Add event_attendees.csv, read all lines & print first_Names in the list

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,12 @@
 puts 'Event Manager Initialized!'
+if File.exist? 'event_attendees.csv' 
+  #contents = File.read('event_attendees.csv')
+  #puts contents
+
+  len = 0
+  lines = File.readlines('event_attendees.csv')
+  lines.each_with_index do |line, index|
+    columns = line.split(',')
+    puts columns[2] if index > 0
+  end
+end
