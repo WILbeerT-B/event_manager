b530485532ffb577612170a0fa0563ed1800a716
WILbeerT-B <wilbertbundalian@gmail.com>
2024-02-19 19:49:06 -0800

    Add Iterations 0-3

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,149 @@
-puts 'Event Manager Initialized!'
-if File.exist? 'event_attendees.csv' 
-  #contents = File.read('event_attendees.csv')
-  #puts contents
+file = 'event_attendees.csv'
 
-  len = 0
-  lines = File.readlines('event_attendees.csv')
+#Iteration 0: Loading a file
+#===========================
+
+#puts 'Event Manager Initialized!'
+#check if file exist
+#if File.exist? file 
+
+#reading the whole file
+#contents = File.read(file)
+#puts contents
+
+=begin
+  #read the file line by line
+  lines = File.readlines(file)
   lines.each_with_index do |line, index|
+    
+    #skip the header 
+    next if index == 0  
     columns = line.split(',')
-    puts columns[2] if index > 0
+    first_name = columns[2] 
+    puts first_name
   end
 end
+=end
+
+#Iteration 1: Parsing with CSV
+#=============================
+
+=begin
+
+require 'csv'
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
+
+=end
+
+#Iteration 2: Cleaning up our zip codes
+#======================================
+=begin
+
+require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
+#refactored method
+#def clean_zipcode(zipcode)
+  #zipcode.to_s.rjust(5, '0')[0..4]
+#end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  file,
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+
+  
+
+  puts "#{name} #{zipcode}"
+end
+=end
+
+
+#Iteration 3: Using Google’s Civic Information
+#=============================================
+
+=begin
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
+end
+=end
+
+#Iteration 4: Form Letters
+#=========================
+
